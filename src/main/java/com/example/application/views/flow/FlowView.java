48fdd8e8ed2af9c670118c3164aee2e877aa5680
Sergey Vinogradov <mr.vursen@gmail.com>
2026-02-17 11:28:07 +0400

    use 25.1-SNAPSHOT, enable slider feature flag

--- src/main/java/com/example/application/views/flow/FlowView.java
+++ src/main/java/com/example/application/views/flow/FlowView.java
@@ -1,8 +1,6 @@
 package com.example.application.views.flow;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.Direction;
-import com.vaadin.flow.component.UI;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
